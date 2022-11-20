520c09a7c244dd4acae57179308b5c3a2cc35ecb
Vitalii Mytenko <vetalmit@gmail.com>
2022-11-20 19:16:20 +0200

    Fix schemas

--- currency/schemas.py
+++ currency/schemas.py
@@ -2,16 +2,29 @@
 
 from ninja import ModelSchema, Schema
 
-from currency.models import Currency, Offer
+from currency.models import Currency, Deal, Offer
+
+# class CurrenciesOut(ModelSchema):
+#     class Config:
+#         model = Currency
+#         model_fields = ["id", "code", "name", "image"]
 
 
-class CurrenciesOut(ModelSchema):
-    class Config:
-        model = Currency
-        model_fields = ["id", "code", "name", "image"]
+class CurrencyBase(Schema):
+    """Base currency schema for GET method, response."""
+    id: int
+    code: str
+    name: str
+    image: str
+
+
+class CurrencyIn(CurrencyBase):
+    """Currency schema for POST method."""
+    id: int = None
 
 
 class OfferIn(Schema):
+    """Offer schema for POST method."""
     currency_to_sell_id: int
     currency_to_buy_id: int
     user_id: int
@@ -19,13 +32,6 @@ class OfferIn(Schema):
     exchange_rate: float
 
 
-# class OfferIn(ModelSchema):
-#     class Config:
-#         model = Offer
-#         model_fields = [
-#             "currency_to_sell",
-#             "currency_to_buy",
-#             "user",
-#             "amount",
-#             "exchange_rate",
-#         ]
+class ErrorMsg(Schema):
+    """Base schema for error message response."""
+    message: str
