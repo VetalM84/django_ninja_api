358bdd864d6416964a249a73e4143e1fbc98902a
Vitalii Mytenko <vetalmit@gmail.com>
2022-11-21 21:26:31 +0200

    Add PUT and GET methods for Offer

--- currency/api.py
+++ currency/api.py
@@ -7,7 +7,7 @@ from ninja import NinjaAPI
 from ninja.pagination import paginate
 
 from currency.models import Currency, Offer
-from currency.schemas import CurrencyBase, CurrencyIn, ErrorMsg, OfferIn
+from currency.schemas import CurrencyBase, CurrencyIn, ErrorMsg, OfferIn, OfferBase
 
 api = NinjaAPI()
 
@@ -18,32 +18,21 @@ def server_status(request):
     return {"Server": "running..."}
 
 
-@api.get(
-    "/currency/{currency_id}",
-    response=CurrencyBase,
-    tags=["Currency"],
-)
+@api.get("/currencies/{currency_id}", response=CurrencyBase, tags=["Currency"])
 def get_currency(request, currency_id: int):
     """Get single currency."""
     return get_object_or_404(Currency, pk=currency_id)
 
 
-@api.get(
-    "/currency/all/",
-    response={200: List[CurrencyBase], 404: ErrorMsg},
-    tags=["Currency"],
-)
+@api.get("/currencies", response=List[CurrencyBase], tags=["Currency"])
 @paginate()
 def get_currencies(request):
     """Get all currencies."""
-    currency_list = Currency.objects.all()
-    if len(currency_list) == 0:
-        return 404, {"message": "Nothing found"}
     # currency_list = Currency.objects.annotate(offers_count=Count("currencies_to_sell"))
-    return currency_list
+    return Currency.objects.all()
 
 
-@api.post("/currency/", response={201: CurrencyBase, 400: ErrorMsg}, tags=["Currency"])
+@api.post("/currencies", response={201: CurrencyBase, 400: ErrorMsg}, tags=["Currency"])
 def add_currency(request, payload: CurrencyIn):
     """Add new currency."""
     if Currency.objects.filter(code__iexact=payload.code).exists():
@@ -51,11 +40,7 @@ def add_currency(request, payload: CurrencyIn):
     return 201, Currency.objects.create(**payload.dict())
 
 
-@api.put(
-    "/currency/{currency_id}",
-    response={200: CurrencyBase},
-    tags=["Currency"],
-)
+@api.put("/currencies/{currency_id}", response={200: CurrencyBase}, tags=["Currency"])
 def edit_currency(request, currency_id: int, payload: CurrencyIn):
     """Edit currency."""
     currency = get_object_or_404(Currency, pk=currency_id)
@@ -65,21 +50,33 @@ def edit_currency(request, currency_id: int, payload: CurrencyIn):
     return 200, {**payload.dict()}
 
 
-@api.delete("/currency/{currency_id}", tags=["Currency"])
+@api.delete("/currencies/{currency_id}", tags=["Currency"])
 def delete_currency(request, currency_id: int):
     """Delete currency."""
     get_object_or_404(Currency, pk=currency_id).delete()
     return {"success": True}
 
 
-@api.post("/offer/", tags=["Offer"])
+@api.get("/offers/{offer_id}", response=OfferBase, tags=["Offer"])
+def get_offer(request, offer_id: int):
+    """Get single offer."""
+    return get_object_or_404(Offer, pk=offer_id)
+
+
+@api.get("/offers", response=List[OfferBase], tags=["Offer"])
+@paginate()
+def get_all_offers(request):
+    """Get all offers."""
+    return Offer.objects.all()
+
+
+@api.post("/offers", response=OfferBase, tags=["Offer"])
 def add_offer(request, payload: OfferIn):
     """Add new offer."""
-    offer = Offer.objects.create(**payload.dict())
-    return {"id": offer.pk}
+    return Offer.objects.create(**payload.dict())
 
 
-@api.delete("/offer/{offer_id}", tags=["Offer"])
+@api.delete("/offers/{offer_id}", tags=["Offer"])
 def delete_offer(request, offer_id: int):
     """Delete offer."""
     offer = get_object_or_404(Offer, pk=offer_id)
--- currency/schemas.py
+++ currency/schemas.py
@@ -1,5 +1,7 @@
 """Data serialization for API."""
 
+from datetime import datetime
+
 from ninja import ModelSchema, Schema
 
 from currency.models import Currency, Deal, Offer
@@ -12,6 +14,7 @@ from currency.models import Currency, Deal, Offer
 
 class CurrencyBase(Schema):
     """Base currency schema for GET method, response."""
+
     id: int
     code: str
     name: str
@@ -20,18 +23,36 @@ class CurrencyBase(Schema):
 
 class CurrencyIn(CurrencyBase):
     """Currency schema for POST method."""
+
     id: int = None
 
 
-class OfferIn(Schema):
-    """Offer schema for POST method."""
+class OfferBase(Schema):
+    """Base offer schema for GET method, response."""
+
+    id: int
     currency_to_sell_id: int
     currency_to_buy_id: int
-    user_id: int
     amount: float
     exchange_rate: float
+    user_id: int
+    added_time: datetime = None
+    active_state: bool = True
+
+
+class OfferIn(OfferBase):
+    """Offer schema for POST method."""
+
+    id: int = None
+
+
+class OfferState(OfferBase):
+    """Offer state for POST method to enable/disable an offer."""
+
+    active_state: bool
 
 
 class ErrorMsg(Schema):
     """Base schema for error message response."""
+
     message: str
