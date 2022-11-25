f9ab3ea4adf3283e74c08c12debda0de52fce126
Vitalii Mytenko <vetalmit@gmail.com>
2022-11-25 22:54:47 +0200

    Add test

--- currency/tests.py
+++ currency/tests.py
@@ -1,3 +1,19 @@
+"""Test cases for Django API framework."""
+
 from django.test import TestCase
 
-# Create your tests here.
+
+class TestAPI(TestCase):
+
+    def setUp(self):
+        """Set up method."""
+        print("SetUp")
+
+    def tearDown(self):
+        """Tear down method."""
+        print("TearDown")
+
+    def test_server_status(self):
+        response = self.client.get(path="/api/")
+        self.assertEqual(response.status_code, 200)
+        self.assertJSONEqual(response.content, {"Server": "running..."})
