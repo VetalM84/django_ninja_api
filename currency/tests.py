81848d2e8bab06231599fe3af9e2e36cd909d749
Vitalii Mytenko <vetalmit@gmail.com>
2022-11-26 09:08:40 +0200

    Add test

--- currency/tests.py
+++ currency/tests.py
@@ -2,8 +2,27 @@
 
 from django.test import TestCase
 
+from currency.models import Currency
+
 
 class TestAPI(TestCase):
+    """Ninja API testing methods."""
+
+    @classmethod
+    def setUpClass(cls):
+        """Setup class."""
+        super().setUpClass()
+        Currency.objects.create(
+            id=1,
+            code="EUR",
+            name="Euro",
+            image="default_image.jpg",
+        )
+
+    @classmethod
+    def tearDownClass(cls):
+        """Tear down class."""
+        print("tearDownClass")
 
     def setUp(self):
         """Set up method."""
@@ -14,6 +33,21 @@ class TestAPI(TestCase):
         print("TearDown")
 
     def test_server_status(self):
+        """Test server status response."""
         response = self.client.get(path="/api/")
         self.assertEqual(response.status_code, 200)
-        self.assertJSONEqual(response.content, {"Server": "running..."})
+        self.assertJSONEqual(
+            raw=response.content, expected_data={"Server": "running..."}
+        )
+
+    def test_get_single_currency(self):
+        """Test GET single currency."""
+        response = self.client.get(path="/api/currencies/1")
+        content = {
+            "id": 1,
+            "code": "EUR",
+            "name": "Euro",
+            "image": "/media/default_image.jpg",
+        }
+        self.assertEqual(response.status_code, 200)
+        self.assertJSONEqual(raw=response.content, expected_data=content)
