5d968d2c0c520a3d7e7f963631b5de6b4a5ddc8b
Vitalii Mytenko <vetalmit@gmail.com>
2022-11-26 15:51:15 +0200

    Add tests

--- currency/tests.py
+++ currency/tests.py
@@ -12,12 +12,22 @@ class TestAPI(TestCase):
     def setUpClass(cls):
         """Setup class."""
         super().setUpClass()
-        Currency.objects.create(
-            id=1,
-            code="EUR",
-            name="Euro",
-            image="default_image.jpg",
-        )
+        currency_data = [
+            {
+                "id": 1,
+                "code": "EUR",
+                "name": "Euro",
+                "image": "eur.jpg",
+            },
+            {
+                "id": 2,
+                "code": "USD",
+                "name": "US Dollar",
+                "image": "usd.jpg",
+            },
+        ]
+        currency_list = [Currency(**data_dict) for data_dict in currency_data]
+        Currency.objects.bulk_create(currency_list)
 
     @classmethod
     def tearDownClass(cls):
@@ -47,7 +57,70 @@ class TestAPI(TestCase):
             "id": 1,
             "code": "EUR",
             "name": "Euro",
-            "image": "/media/default_image.jpg",
+            "image": "/media/eur.jpg",
         }
         self.assertEqual(response.status_code, 200)
         self.assertJSONEqual(raw=response.content, expected_data=content)
+
+    def test_get_all_currencies(self):
+        """Test GET all currencies."""
+        response = self.client.get(path="/api/currencies")
+        self.assertEqual(response.status_code, 200)
+
+    def test_add_new_currency(self):
+        """Test POST new currency."""
+        data = {
+            "code": "UAH",
+            "name": "Hryvna",
+            "image": "/uah.jpg",
+        }
+        response = self.client.post(
+            path="/api/currencies", data=data, content_type="application/json"
+        )
+        self.assertEqual(response.status_code, 201)
+
+    def test_add_existing_currency(self):
+        """Test POST existing currency."""
+        data = {
+            "code": "USD",
+            "name": "Hryvna",
+            "image": "/uah.jpg",
+        }
+        response = self.client.post(
+            path="/api/currencies", data=data, content_type="application/json"
+        )
+        self.assertEqual(response.status_code, 400)
+
+    def test_edit_currency(self):
+        """Test PUT currency."""
+        data = {
+            "code": "UAH",
+            "name": "Hryvna",
+            "image": "/uah.jpg",
+        }
+        response = self.client.put(
+            path="/api/currencies/1", data=data, content_type="application/json"
+        )
+        self.assertEqual(response.status_code, 200)
+
+    def test_edit_currency_fail(self):
+        """Test PUT currency fail."""
+        data = {
+            "code": "UAH",
+            "name": "Hryvna",
+            "image": "/uah.jpg",
+        }
+        response = self.client.put(
+            path="/api/currencies/111", data=data, content_type="application/json"
+        )
+        self.assertEqual(response.status_code, 404)
+
+    def test_delete_currency(self):
+        """Test DELETE currency."""
+        response = self.client.delete(path="/api/currencies/1")
+        self.assertEqual(response.status_code, 204)
+
+    def test_delete_currency_fail(self):
+        """Test DELETE currency fail."""
+        response = self.client.delete(path="/api/currencies/111")
+        self.assertEqual(response.status_code, 404)
